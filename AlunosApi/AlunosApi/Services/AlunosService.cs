f749f27ffd0d3a868a5bb649f84bb2f458208527
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-05-24 13:06:00 -0300

    Criação da interface IAlunService e da classe concreta que implementa essa interface

---
+++ AlunosApi/AlunosApi/Services/AlunosService.cs
@@ -0,0 +1,36 @@
+﻿using AlunosApi.Models;
+
+namespace AlunosApi.Services;
+
+public class AlunosService : IAlunoService
+{
+    public Task<IEnumerable<Aluno>> GetAlunos()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Aluno> GetAluno(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Aluno>> GetAlunoByNome(string nome)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CreateAluno(Aluno aluno)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAluno(Aluno aluno)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAluno(Aluno aluno)
+    {
+        throw new NotImplementedException();
+    }
+}
